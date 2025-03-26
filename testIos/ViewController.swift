37357dbdfa4e6c035193548b3e0282811622adee
yhj0904 <ujk6073@gmail.com>
2025-03-26 10:26:23 +0900

    Initial Commit

---
+++ testIos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testIos
+//
+//  Created by 윤형주 on 3/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testIosTests/testIosTests.swift
@@ -0,0 +1,17 @@
+//
+//  testIosTests.swift
+//  testIosTests
+//
+//  Created by 윤형주 on 3/26/25.
+//
+
+import Testing
+@testable import testIos
+
+struct testIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
