5ceeff565fefac0615498ec50e43506baec1b6a4
yhj0904 <ujk6073@gmail.com>
2025-03-26 14:19:41 +0900

    :tada: first commit ! Hello world !!!

--- testIos/ViewController.swift
+++ testIos/ViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel?.text = "hello world"
         // Do any additional setup after loading the view.
     }
 
