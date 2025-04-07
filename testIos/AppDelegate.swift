bc30ea8468eca2e8c283f057ce99b5f312f2fb52
yhj0904 <ujk6073@gmail.com>
2025-04-07 16:27:12 +0900

    :construction: :triangular_flag_on_post: PUSH notification feature update ...

--- testIos/AppDelegate.swift
+++ testIos/AppDelegate.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import UserNotifications
+import FirebaseCore
+import FirebaseMessaging
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //파이어베이스 설정
+        FirebaseApp.configure()
+        
+        // 앱 실행 시 유저에게 알림 허용 권한.
+        UNUserNotificationCenter.current().delegate = self
+        // 파이어베이스 Messaging 설정
+        Messaging.messaging().delegate = self
+
+        //필요한 알림 권한 설정
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+        
+        //UNUserNotificationCenterDelegate를 구현한 메서드 실행
+        application.registerForRemoteNotifications()
+        
         return true
     }
 
@@ -30,7 +51,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // 파이어베이스 MessagingDelegate 설정
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+        UserDefaults.standard.set(fcmToken, forKey: "token")
+    }
+}
