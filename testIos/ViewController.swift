896cbdc19d4b94e3596e5baf17f77eb2733349be
yhj0904 <ujk6073@gmail.com>
2025-04-16 18:01:49 +0900

    :sparkles: webview update ...

--- testIos/ViewController.swift
+++ testIos/ViewController.swift
@@ -6,23 +6,30 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let label = UILabel()
-        label.text = "hello world"
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 36)
-        label.textColor = .black
-        label.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 50)
+//        let label = UILabel()
+//        label.text = "hello world"
+//        label.textAlignment = .center
+//        label.font = UIFont.systemFont(ofSize: 36)
+//        label.textColor = .black
+//        label.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 50)
+//        
+//        view.addSubview(label)
+//        // Do any additional setup after loading the view.
+        webView = WKWebView(frame: self.view.frame)
+        self.view.addSubview(webView)
         
-        view.addSubview(label)
-        // Do any additional setup after loading the view.
+        if let url = URL(string: "http://192.168.10.205"){
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
-
-
 }
-
